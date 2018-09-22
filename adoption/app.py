9ce2bcf6a440eb3173c8c3dcf2c79475eb1757df
cristian ordoñez <cristiano_ord93@hotmail.com>
2018-09-22 00:16:50 -0500

    Add ORM

--- adoption/app.py
+++ adoption/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 from adoption.config.development import DevelopmentConfig
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
+
+    # Init database
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
 
     return app
--- adoption/config/development.py
+++ adoption/config/development.py
@@ -1,5 +1,11 @@
+import os
+
 from .default import DefaultConfig
+
+BASE_DIR = os.getcwd()
 
 
 class DevelopmentConfig(DefaultConfig):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(BASE_DIR, 'db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
