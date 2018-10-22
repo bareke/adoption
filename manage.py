4f0ecff900ac2d244a597c7bbc53c5dbfaf592cf
cristian ordoñez <cristiano_ord93@hotmail.com>
2018-10-22 11:14:54 -0500

    Populate database with fake data

--- manage.py
+++ manage.py
@@ -1,22 +1,54 @@
 from flask_script import Manager
 from flask_migrate import Migrate
 from flask_migrate import MigrateCommand
+from mixer.backend.flask import Mixer
 
 from adoption.app import db
 from adoption.app import create_app
+from adoption.models import *
 
-migrate = Migrate()
+app = create_app()
+manager = Manager(app)
 
 
-def create_manager():
-    app = create_app()
-    migrate.init_app(app, db)
-    manager = Manager(app)
-    manager.add_command('db', MigrateCommand)
+@manager.option('-c', '--count', help='populate database with fake data')
+def populate(count):
+    mixer = Mixer(app)
+    count = int(count)
 
-    return manager
+    models = [Client, Contact]
+
+    for model in models:
+        mixer.cycle(count).blend(model)
+
+    mixer.cycle(count).blend(Pet,
+                             neutering=mixer.sequence(True, False, None),
+                             gender=mixer.sequence('m', 'f')
+                             )
+
+    pets = Pet.query.limit(count)
+    clients = Client.query.limit(count)
+
+    mixer.cycle(count).blend(Register,
+                             comment=mixer.RANDOM,
+                             old_owner=(client for client in clients),
+                             old_pet=(pet for pet in pets),
+                             )
+    mixer.cycle(count).blend(Adoption,
+                             status='in process',
+                             new_owner=(client for client in clients),
+                             new_pet=(pet for pet in pets)
+                             )
+
+    mixer.cycle(count).blend(Image, pet=(pet for pet in pets))
+    mixer.cycle(count).blend(Image, client=(client for client in clients))
+
+    clients = Client.query.all()
+    mixer.cycle(len(clients)).blend(User, client=(client for client in clients))
 
 
 if __name__ == '__main__':
-    manager = create_manager()
+    migrate = Migrate(app, db)
+    manager.add_command('db', MigrateCommand)
+
     manager.run()
